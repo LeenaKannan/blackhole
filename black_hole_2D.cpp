d0c26d947d2339c8ca4b88c16ee09a636ecdc70d
LeenaKannan <leenakannan.23@gmail.com>
2025-08-13 02:03:33 +0530

    wrote a few lines, it's 3 am, will continue tomorrow

---
+++ black_hole_2D.cpp
@@ -0,0 +1,20 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include <vector>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+using namespace glm;
+using namespace std;
+
+double c = 299792458.0;
+double G = 6.67430e-11;
+
+struct Ray;
+void rk4Step(Ray &ray, double dλ, double rs);
