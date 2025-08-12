e87f276e58ac7c7c86aa6025846abfede37cd93c
LeenaKannan <leenakannan.23@gmail.com>
2025-08-13 02:19:40 +0530

    continued anyway, engine struct to be completed

--- black_hole_2D.cpp
+++ black_hole_2D.cpp
@@ -1,5 +1,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <cstdio>
+#include <cstdlib>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -18,3 +20,25 @@ double G = 6.67430e-11;
 
 struct Ray;
 void rk4Step(Ray &ray, double dλ, double rs);
+
+struct Engine {
+  GLFWwindow *window;
+  int WIDTH = 800;
+  int HEIGHT = 600;
+  float width = 100000000000.0f; // Width of the viewport in meters
+  float height = 75000000000.0f; // Height of the viewport in meters
+
+  // Navigation state
+  float offsetX = 0.0f, offsetY = 0.0f;
+  float zoom = 1.0f;
+  bool middleMousePressed = false;
+  double lastMouseX = 0.0, lastMouseY = 0;
+
+  Engine() {
+    if (!glfwInit()) {
+      cerr << "failed to initialise glfw" << endl;
+      exit(EXIT_FAILURE);
+    }
+    window = glfwCreateWindow(WIDTH, HEIGHT, "2D Black Hole", NULL, NULL);
+  }
+}
